4b9e698d57091a3e8fcdef452e45c9696aeb08e9
Evgesha <Slav.novikova2014@gmail.com>
2023-05-24 13:49:32 +0700

    src/test/java/ru/netology/stats/StatsServiceTest.java added

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class StatsServiceTest {
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+
+    public void SumOfAllSalesTest() {
+
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.SumOfAllSales(sales);
+        Assertions.assertEquals(expected, actual);
+        
+    }
+
+    @Test
+    public void AverageAmountOfSalesTest() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.AverageAmountOfSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxSalesTest() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.maxSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSalesTest() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.minSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void BelowAverageSalesTest() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.BelowAverageSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void AboveAverageSalesTest() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.AboveAverageSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
